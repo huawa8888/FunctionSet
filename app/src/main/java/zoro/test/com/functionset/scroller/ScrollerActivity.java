2f7eb034474ee94cec74761ce86439a2be81fffc
金樑樑 <1244347482@qq.com>
2017-09-20 09:25:14 +0800

    2017/9/20  添加scroller知识点模块

---
+++ app/src/main/java/zoro/test/com/functionset/scroller/ScrollerActivity.java
@@ -0,0 +1,21 @@
+package zoro.test.com.functionset.scroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import zoro.test.com.functionset.R;
+
+/**
+ * @Author : Zoro.
+ * @Date : 2017/9/19.
+ * @Describe :
+ */
+
+public class ScrollerActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroller);
+    }
+}
